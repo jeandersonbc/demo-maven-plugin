41f0b9a679561b03022df3e19b99a7d7fe210eaa
Jeanderson Candido <jeandersonbc@gmail.com>
2018-04-23 23:33:34 -0300

    Added mojo parameter

--- src/main/java/gh/jbc/maven/demo/DemoMojo.java
+++ src/main/java/gh/jbc/maven/demo/DemoMojo.java
@@ -4,12 +4,16 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "sayhi")
 public class DemoMojo extends AbstractMojo {
 
+	@Parameter(property = "sayhi.greeting", defaultValue = "Hello World!")
+	private String greeting;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("Hello World!");
+		getLog().info(greeting);
 	}
 }
